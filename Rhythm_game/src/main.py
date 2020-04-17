3b41c742474978918a5bfd51a29bfc8a62af4815
MicroOptimization <jackiezhen538@gmail.com>
2020-04-17 09:24:46 -0400

    adding debug aids for aim accuracy

--- Rhythm_game/src/main.py
+++ Rhythm_game/src/main.py
@@ -18,8 +18,9 @@ class Application(tk.Frame):
         self.pack()
         
         self.entities = []
+        root.update()
         
-        self.update()
+        self.final_radius = 69
         
         self.canvas.bind("<Key>", self.key_listener)
         self.canvas.bind("<Button-1>", self.focuser)
@@ -28,27 +29,10 @@ class Application(tk.Frame):
         self.canvas.pack()
         
         self.create_bounds()
-        
-    def create_bounds(self):
-        self.tightest_ub = 400 + (100 / 2)
-        self.tightest_lb = 400 + ((100 - self.final_radius) / 2)
-        
-        self.e_ub = self.tightest_ub
-        self.e_lb = self.tightest_lb
-        
-        self.g_ub = self.tightest_ub - 10
-        self.g_lb = self.tightest_lb + 10
-        
-        self.o_ub = self.tightest_ub - 20
-        self.o_lb = self.tightest_lb + 20
-        
-        self.m_ub = self.tightest_lb - 50
-        self.m_lb = self.tightest_lb + 50
-        
+        self.update()
         
     def draw_target(self):
         root.update()
-        self.final_radius = 69
         
         self.ring_x0 = (self.canvas.winfo_width() - self.final_radius) / 2
         self.ring_y0 = (self.canvas.winfo_height() - self.final_radius) - 54 
@@ -62,7 +46,6 @@ class Application(tk.Frame):
         
         self.hit_msg = ""
         
-        
     def key_listener(self, event):
         key = repr(event.char) 
         if key == "'q'":
@@ -73,23 +56,50 @@ class Application(tk.Frame):
                     ring = i
                     print("Ring pos: " , ring.y)
                     msg = self.check_accuracy(ring.y)
+
+    def create_bounds(self):
+        #Setting bounds for target rings
+        
+        self.tightest_ub = (self.canvas.winfo_height() / 2)
+        self.tightest_lb = ((self.canvas.winfo_height() - self.final_radius) / 2)
+        
+        self.e_ub = self.tightest_ub
+        self.e_lb = self.tightest_lb
+        
+        self.g_ub = self.tightest_ub + 10
+        self.g_lb = self.tightest_lb - 10
+        
+        self.o_ub = self.tightest_ub + 20
+        self.o_lb = self.tightest_lb - 20
+        
+        self.m_ub = self.tightest_lb + 50
+        self.m_lb = self.tightest_lb - 50
     
+
     def create_bound_debug_lines(self):
-        canvas.create_line(0, self.e_ub, canvas.winfo_width(), self.e_ub, fill="blue")
+        canvas.create_line(0, self.e_ub, canvas.winfo_width(), self.e_ub, fill="blue", width=2)
+        canvas.create_line(0, self.e_lb, canvas.winfo_width(), self.e_lb, fill="blue", width=2)
+        
+        canvas.create_line(0, self.g_ub, canvas.winfo_width(), self.g_ub, fill="red", width=2)
+        canvas.create_line(0, self.g_lb, canvas.winfo_width(), self.g_lb, fill="red", width=2)
+        
+        canvas.create_line(0, self.o_ub, canvas.winfo_width(), self.o_ub, fill="green", width=2)
+        canvas.create_line(0, self.o_lb, canvas.winfo_width(), self.o_lb, fill="green", width=2)
+        
+        canvas.create_line(0, self.m_ub, canvas.winfo_width(), self.m_ub, fill="black", width=2)
+        canvas.create_line(0, self.m_lb, canvas.winfo_width(), self.m_lb, fill="black", width=2)
         
         
+    def check_accuracy(self, y):
+        msg = ""
+        
+        print("y: " , y)
+        print()
         print(self.e_lb, self.e_ub)
         print(self.g_lb, self.g_ub)
         print(self.o_lb, self.o_ub)
         print(self.m_lb, self.m_ub)
         
-    def check_accuracy(self, y):
-        #Setting bounds for target rings
-        
-        
-        msg = ""
-        print()
-        print("y: " , y)
         
         if y <= self.e_ub and y >= self.e_lb:
             print("Excellent")
@@ -104,10 +114,7 @@ class Application(tk.Frame):
         else:
             print("You missed")
             msg = "Miss"
-        
-        
-        
-        
+
         return msg
         
     def focuser(self, event):
